3e19e47033aa3d46cf8cbb63ecc59d2554f8a9ca
Khwaja Abdul Samad <thatsme104@gmail.com>
2024-06-11 23:43:45 +0530

    Completed First Version

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -56,22 +56,21 @@ const PostListProvider = ({ children }) => {
 
 const DEFAULT_POST_LIST = [
   {
-    id: '1',
-    title: 'Going to Mumbai',
-    body: 'Hi Friends I am going to Mumbai for my vacations , Hope to enjoy a lot',
-    reactions: 15,
-    userID: 'the__outlier__',
-    tags: ['vacation', 'Mumbai', 'Enjoying'],
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoying"],
   },
   {
-    id: '2',
-    title: 'Graduated!!',
-    body: 'Tumhara bhai ENgineer ban gaya !!',
-    reactions: 30,
-    userID: 'abdul_samad',
-    tags: ['Engineer', 'Graduate', 'JMI'],
+    id: "2",
+    title: "Paas ho bhai",
+    body: "4 saal ki masti k baad bhi ho gaye hain paas. Hard to believe.",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
   },
-]
-
+];
 
 export default PostListProvider;
