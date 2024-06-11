728fb8379e1cd3f4a075a8a190c8ecb2a103823f
Khwaja Abdul Samad <thatsme104@gmail.com>
2024-06-11 21:02:56 +0530

    Added Username , Content , Title to post

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,17 +1,38 @@
 const CreatePost = () => {
   return <form className="create-post">
     <div className="mb-3">
-      <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-      <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+      <label htmlFor="userId" className="form-label">
+       Enter Your User ID here
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="title"
+        placeholder="How are you feeling today..."
+      />
     </div>
     <div className="mb-3">
-      <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-      <input type="password" className="form-control" id="exampleInputPassword1" />
+      <label htmlFor="title" className="form-label">
+        Post Title
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="title"
+        placeholder="How are you feeling today..."
+      />
     </div>
-    <div className="mb-3 form-check">
-      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-      <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+    <div className="mb-3">
+      <label htmlFor="title" className="form-label">
+        Post Content
+      </label>
+      <textarea
+        type="text"
+        rows="4"
+        className="form-control"
+        id="body"
+        placeholder="Tell us more about it"
+      />
     </div>
     <button type="submit" className="btn btn-primary">Submit</button>
   </form>
