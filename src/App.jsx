cdb6a268198b1390cbc72040045f65e9702a8197
Khwaja Abdul Samad <thatsme104@gmail.com>
2024-06-11 20:35:29 +0530

    Fourth commit: Added Post PostList and CreatePost template

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Sidebar from './components/components/Sidebar'
-import Header from './components/components/Header'
-import Footer from './components/components/Footer'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
+import PostListProvider from "./store/post-list-store";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
-    <>
-      <div className="container">
-        <Sidebar></Sidebar>
-        <div>
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></Sidebar>
+        <div className="content">
           <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
           <Footer></Footer>
         </div>
       </div>
-    </>
-  )
+    </PostListProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/components/CreatePost.jsx
+++
@@ -1,5 +0,0 @@
-const CreatePost = () => {
-  return
-}
-
-export default CreatePost;
