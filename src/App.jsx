027f105d147a43a2e3d33e6ece0eaa0532a38498
Khwaja Abdul Samad <thatsme104@gmail.com>
2024-06-11 19:15:00 +0530

    Third Commit : Applied Header Footer SideBar Template

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './components/components/Sidebar'
+import Header from './components/components/Header'
+import Footer from './components/components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <div className="container">
+        <Sidebar></Sidebar>
+        <div>
+          <Header></Header>
+          <Footer></Footer>
+        </div>
+      </div>
     </>
   )
 }
